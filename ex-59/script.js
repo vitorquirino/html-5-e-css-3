a7f0561c8748c87368e9d5ce60def88a0373855c
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-10-04 14:45:53 -0300

    add

---
+++ ex-59/script.js
@@ -0,0 +1,13 @@
+let tela = document.getElementById("tela");
+let ctx = tela.getContext("2d");
+
+ctx.moveTo(0,0);
+ctx.lineTo(250,250)
+ctx.lineTo(500,0)
+ctx.lineWidth = 3
+ctx.strokeStyle = "white"
+ctx.stroke()
+
+
+
+
