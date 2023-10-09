e62489f2e9640fc0c562fc99b303a5448d4f7953
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-10-09 10:33:59 -0300

    atualização

--- ex-60/script.js
+++ ex-60/script.js
@@ -2,6 +2,16 @@ let tela = document.getElementById("tela");
 
 let ctx = tela.getContext("2d");
 
-ctx.moveTo(0,0) 
-ctx.lineTo(200,200 )
+ctx.rect(0,10, 100, 200)
+
+ctx.fillStyle = "blue"
+//ctx.fillRect(10,10, 100, 200)
+ctx.lineWidth = 5
+
+ctx.strokeStyle = "red"
+//ctx.strokeRect(10,10, 100, 200)
+
+ctx.fill()
 ctx.stroke()
+
+ctx.clearRect(20,20,30,30)
