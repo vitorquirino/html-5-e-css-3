3e5004bf9d7dc35c199ef5dc2eafc9af8452cafa
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-11-30 21:21:07 -0300

    add

--- ex-70/script.js
+++ ex-70/script.js
@@ -4,9 +4,33 @@ const play = document.getElementById('play')
 
 songName.innerText = 'United In Grief'
 
+let isPlaying = false;
+
 function playSong(){
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
     musica.play();
+
+    isPlaying = true;
 }
 
-play.addEventListener('click',playSong);
+function pauseSong(){
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+    musica.pause();
+
+    isPlaying = false
+}
+
+function playPauseDecider(){
+    if(isPlaying === true){
+        pauseSong();
+    }
+    else{
+        playSong();
+    }
+}
+
+
+play.addEventListener('click',playPauseDecider);
 
