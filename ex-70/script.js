887b9a853534135fbbbce5b9afc55b2176249157
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-12-04 21:26:24 -0300

    add

--- ex-70/script.js
+++ ex-70/script.js
@@ -1,8 +1,26 @@
 const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name')
 const musica = document.getElementById('audio');
+const cover = document.getElementById('cover');
 const play = document.getElementById('play')
 
-songName.innerText = 'United In Grief'
+const UnitedInGrief ={
+    songName : 'United-In-Grief',
+    artist : 'kendrick-Lamar',
+    file : 'capa-in-grief.jpg'
+};
+
+const wannabesedated ={
+    songName : 'wanna-be-sedated',
+    artist : 'ramones',
+    file : 'capa-wanna-be-sedated.jpg'
+};
+
+const Mockingbird ={
+    songName : 'Mockingbird',
+    artist : 'eminem',
+    file : 'capa-Mockingbird.jpg'
+};
 
 let isPlaying = false;
 
